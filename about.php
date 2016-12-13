44ef9d840b98e38cfd78f40f49651b9b6c25e6cd
erick sutedy <erick.stdy@gmail.com>
2016-12-13 14:31:35 +0700

    rapiin dikit
    rapiin dikit

--- about.php
+++ about.php
@@ -1,19 +1,26 @@
 <section class="mbr-box mbr-section mbr-section--relative mbr-section--fixed-size mbr-section--full-height mbr-section--bg-adapted mbr-parallax-background" id="header1-43" style="background-image: url(assets/images/iphone-926663-1920-1920x1280-94.jpg);background-color: rgba(0, 0, 0, 0.4); */">
     <div class="mbr-box__magnet mbr-box__magnet--sm-padding mbr-box__magnet--center-left">
-        <center><h3 class="mbr-hero__text wow lightSpeedIn" style="color:white;"></h3></center>
+        <center>
+            <h3 class="mbr-hero__text wow lightSpeedIn" style="color:white;"></h3>
+        </center>
         <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(41, 43, 49);"></div>
         <div class="mbr-box__container mbr-section__container container">
-            <div class="mbr-box mbr-box--stretched"><div class="mbr-box__magnet mbr-box__magnet--center-left">
-                <div class="row"><div class=" col-sm-6">
-                    <div class="mbr-hero wow fadeInUp">
-                        <h1 class="mbr-hero__text"><img src="assets/images/logo_2016.png" class="img-responsive"></h1>
-                        <p class="mbr-hero__subtext">Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></p>
+            <div class="mbr-box mbr-box--stretched">
+                <div class="mbr-box__magnet mbr-box__magnet--center-left">
+                    <div class="row">
+                        <div class=" col-sm-6">
+                            <div class="mbr-hero wow fadeInUp">
+                                <h1 class="mbr-hero__text">
+                                    <img src="assets/images/logo_2016.png" class="img-responsive">
+                                </h1>
+                                <p class="mbr-hero__subtext">Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></p>
+                            </div>
+                            <div class="mbr-buttons btn-inverse mbr-buttons--left"></div>
+                        </div>
                     </div>
-                    <div class="mbr-buttons btn-inverse mbr-buttons--left"></div>
-                </div></div>
-            </div></div>
+                </div>
+            </div>
         </div>
-        
     </div>
 </section>
 
@@ -24,7 +31,9 @@
         <div class="row" style="padding-top: 14px;padding-bottom: 14px;border-radius:5px;">
             <div class="mbr-box mbr-box--fixed mbr-box--adapted">
                 <div class="mbr-box__magnet mbr-box__magnet--top-right mbr-section__left col-sm-6 wow fadeInLeft">
-                    <figure class="mbr-figure mbr-figure--adapted mbr-figure--caption-inside-bottom mbr-figure--full-width"><img class="mbr-figure__img" src="assets/images/imac-605421-1920-1920x1275-41.jpg"></figure>
+                    <figure class="mbr-figure mbr-figure--adapted mbr-figure--caption-inside-bottom mbr-figure--full-width">
+                        <img class="mbr-figure__img" src="assets/images/imac-605421-1920-1920x1275-41.jpg">
+                    </figure>
                 </div>
                 <div class="mbr-box__magnet mbr-class-mbr-box__magnet--center-left col-sm-6 mbr-section__right wow fadeInRight">
                     <div class="mbr-section__container mbr-section__container--middle">
@@ -42,17 +51,17 @@
                                 </div>
                             </div>
                             <!--<h3 class="mbr-header__text">Hijab Hunt 2017 Akan Segera Dimulai, Daftarkan Dirimu Segera</h3>-->
-                            
                         </div>
                     </div>
                     <div class="mbr-section__container mbr-section__container--middle">
-                        <div class="mbr-article mbr-article--auto-align mbr-article--wysiwyg"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p></div>
+                        <div class="mbr-article mbr-article--auto-align mbr-article--wysiwyg">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
                     </div>
                     <div class="mbr-section__container">
                         <div class="mbr-buttons mbr-buttons--auto-align btn-inverse"></div>
                     </div>
                 </div>
-                
             </div>
         </div>
     </div>
